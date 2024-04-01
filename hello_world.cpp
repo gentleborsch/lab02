34e671435b66d893ec446f9f239e851367015d45
gentleborsch <paker2859@gmail.com>
2024-04-02 00:53:27 +0300

    changing hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,21 @@
-#include
-using namespace std;
-int main()
-{
-cout << "Hello World!" << endl;
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    
+    // Запрашиваем имя пользователя
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+    
+    // Выводим приветственное сообщение
+    std::cout << "Hello world from " << name << "!" << std::endl;
+    
+    return 0;
 }
+
+
+
+
+
+
