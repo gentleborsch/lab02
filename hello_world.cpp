0f67cfa65cbba6a39559480a4e5df87a35d8fb4a
gentleborsch <165718811+gentleborsch@users.noreply.github.com>
2024-04-02 01:05:39 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string name;
+
+    // Запрашиваем имя пользователя
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+
+    // Выводим приветственное сообщение
+    std::cout << "Hello world from " << name << "!" << std::endl;
+
+    return 0;
+}
